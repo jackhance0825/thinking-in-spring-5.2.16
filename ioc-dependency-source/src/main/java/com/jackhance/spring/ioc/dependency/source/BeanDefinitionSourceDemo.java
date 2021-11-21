02a74b9639c2faa1da52a796062b70ed6590917f
jackhance <jackhance0825@163.com>
2021-11-21 17:39:50 +0800

    dependency source

--- ioc-dependency-source/src/main/java/com/jackhance/spring/ioc/dependency/source/BeanDefinitionSourceDemo.java
+++ ioc-dependency-source/src/main/java/com/jackhance/spring/ioc/dependency/source/BeanDefinitionSourceDemo.java
@@ -1,6 +1,8 @@
 package com.jackhance.spring.ioc.dependency.source;
 
 import com.jackhance.spring.ioc.dependency.source.model.Worker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.support.BeanDefinitionBuilder;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
@@ -17,6 +19,10 @@ import java.util.Map;
  * @mail jackhance0825@163.com
  */
 public class BeanDefinitionSourceDemo {
+
+    @Autowired
+    @Qualifier("worker3")
+    private Worker worker;
 
     public static void main(String[] args) {
 
@@ -37,6 +43,21 @@ public class BeanDefinitionSourceDemo {
 
         Map<String, Worker> workers = applicationContext.getBeansOfType(Worker.class);
         System.out.println("workers = " + workers);
+
+        BeanDefinitionSourceDemo demo = applicationContext.getBean(BeanDefinitionSourceDemo.class);
+        System.out.println("demo.worker = " + demo.worker);
+
+        // 重新注册 worker3(Spring 不建议)
+        BeanDefinition beanDefinition = BeanDefinitionBuilder.genericBeanDefinition(Worker.class)
+                .addPropertyValue("id", 4)
+                .addPropertyValue("name", "jackhance")
+                .getBeanDefinition();
+        applicationContext.registerBeanDefinition("worker3", beanDefinition);
+
+        // 无法改变已 populate 的 bean 属性
+        System.out.println("demo.worker = " + demo.worker);
+        System.out.println("demo.worker = " + applicationContext.getBean("worker3"));
+
 
         // 关闭应用上下文
         applicationContext.close();
